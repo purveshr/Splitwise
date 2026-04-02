1f0c8661f75b070a637e560ed0c3e8762bc79f97
purveshr <purveshrode48@gmail.com>
2026-04-02 06:45:28 +0530

    modified some code

--- src/main/java/com/splitwise/Main.java
+++ src/main/java/com/splitwise/Main.java
@@ -1,15 +1,15 @@
 package com.splitwise;
 
 
-import com.splitwise.practiceCode.pyramid;
+import com.splitwise.practiceCode.PatternBuilder;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter an number :");
+        System.out.print("enter an number : ");
         int n = sc.nextInt();
-        pyramid.printPyramid(n);
+        PatternBuilder.hollowSquare(n);
     }
 }
